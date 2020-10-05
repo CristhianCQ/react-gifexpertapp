a01b00879169cd4545142b7da3fd8f40b46393f5
cristhiancq <cristhian.camil-1993@hotmail.com>
2020-10-04 21:26:37 -0500

    docs agregado

---
+++ docs/precache-manifest.f3c1ac42fc43a646dd8c0e227f0aa39d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8ecd14eff8c7652a031ee577f6f5f76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de5de9a0ccab0f9fb600",
+    "url": "/static/css/main.24ef0358.chunk.css"
+  },
+  {
+    "revision": "dcf09d6ede3b15119fcb",
+    "url": "/static/js/2.a16041ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a16041ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de5de9a0ccab0f9fb600",
+    "url": "/static/js/main.fc656627.chunk.js"
+  },
+  {
+    "revision": "9d3929294d677b1b9ff6",
+    "url": "/static/js/runtime-main.aa9df6a3.js"
+  }
+]);
